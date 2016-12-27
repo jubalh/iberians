8d7a277a8b203de8954b3355efa14db0e8c42e5c
Michael Vetter <jubalh@openmailbox.org>
2016-12-27 17:44:10 +0100

    Randomly generate the board

--- main.lua
+++ main.lua
@@ -1,45 +1,30 @@
 debug = true
 
-local myscale = 0.5
+local myscale = 0.4
 
 function love.load()
 	img = {}
+	-- resources
 	img["brick"] = love.graphics.newImage("assets/images/brick.png")
-	img["desert"] = love.graphics.newImage("assets/images/desert.png")
 	img["iron"] = love.graphics.newImage("assets/images/iron.png")
-	img["water"] = love.graphics.newImage("assets/images/water.png")
 	img["wheat"] = love.graphics.newImage("assets/images/wheat.png")
 	img["wood"] = love.graphics.newImage("assets/images/wood.png")
 	img["wool"] = love.graphics.newImage("assets/images/wool.png")
+	-- empty
+	img["desert"] = love.graphics.newImage("assets/images/desert.png")
+	img["water"] = love.graphics.newImage("assets/images/water.png")
 
 	tile_width = img["brick"]:getWidth()
 	tile_height = img["brick"]:getHeight()
 
-	-- create 2d board array
-	board = {}
-	for x = 1, 5 do
-		board[x] = {}
-		for y = 1, 5 do
-			board[x][y] = {}
-		end
-	end
-
-	-- init game board
-	for x = 2, 4 do
-		board[x][1].tile = "brick"
-		board[x][5].tile = "brick"
-	end
-	for x = 1, 4 do
-		board[x][2].tile = "wood"
-		board[x][4].tile = "wood"
-	end
-	for x = 1, 5 do
-		board[x][3].tile = "iron"
-	end
-
+	-- recalculate height according to scale
+	-- testing
 	tile_height = math.floor(tile_height * myscale)
 	tile_width = math.floor(tile_width * myscale)
+
+	board = createBoard()
 end
+
 
 function love.update(dt)
 end
@@ -68,4 +53,63 @@ function love.keypressed(key)
 	if key == "escape" then
 		love.event.push("quit")
 	end
+end
+
+function setTileByNr(cell, nr)
+	if nr == 1 then
+		cell.tile = "brick"
+	elseif nr == 2 then
+		cell.tile = "iron"
+	elseif nr == 3 then
+		cell.tile = "wheat"
+	elseif nr == 4 then
+		cell.tile = "wood"
+	elseif nr == 5 then
+		cell.tile = "wool"
+	elseif nr == 6 then
+		cell.tile = "desert"
+	end
+end
+
+function createBoard()
+	-- create 2d board array
+	local board = {}
+	for x = 1, 5 do
+		board[x] = {}
+		for y = 1, 5 do
+			board[x][y] = {}
+		end
+	end
+
+	-- generate a list with random numbers from one to 5 standing for each tile variant
+	local r = {}
+	for i = 1, 19 do
+		r[i] = love.math.random(1, 5)
+	end
+
+	-- desert
+	local desertpos = love.math.random(1,19)
+	r[desertpos] = 6
+
+	local i = 1
+
+	-- init game board
+	for x = 2, 4 do
+		setTileByNr(board[x][1], r[i])
+		i = i + 1
+		setTileByNr(board[x][5], r[i])
+		i = i + 1
+	end
+	for x = 1, 4 do
+		setTileByNr(board[x][2], r[i])
+		i = i + 1
+		setTileByNr(board[x][4], r[i])
+		i = i + 1
+	end
+	for x = 1, 5 do
+		setTileByNr(board[x][3], r[i])
+		i = i + 1
+	end
+
+	return board
 end
