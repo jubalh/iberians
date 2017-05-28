c96f52fbfcba2e45f40c397f0e475d6943514a44
Michael Vetter <jubalh@openmailbox.org>
2017-05-28 14:36:14 +0200

    Draw 3 action buttons
    Instead of having one 'action' button we have three for each action now.
    - build
    - trade
    - devel cards
    
    They will later open other menus so one can choose what to build, and a
    trading menu and a overview of develcards one has, to play out.
    
    Adding action-icon, should have happened earlier since it was used
    already!

--- panel.lua
+++ panel.lua
@@ -3,6 +3,7 @@ require("board")
 panel = {}
 icon_img = {}
 local action_menu_btn = {}
+local action_btns = {"build", "trade", "develcards"}
 
 function panel.load()
 	-- resource icons
@@ -26,13 +27,35 @@ function panel.drawBar()
 	love.graphics.setColor(255,255,255)
 
 	-- where to draw the icon
-	local icon_x = x + 100
+	local icon_x = x + 10
 	local icon_y = y - 10
 	local text_y = icon_y + 20
 	-- space after an icon (before text)
 	local icon_distance = 10
 	-- space after text
-	local text_distance = 50
+	local text_distance = 20
+
+	-- calculate the positions of the action buttons (trade, build, develcards)
+	for k, v in ipairs(action_btns) do
+		action_menu_btn[v] = {}
+		action_menu_btn[v].x = icon_x
+		action_menu_btn[v].y = icon_y
+		action_menu_btn[v].w = action_icon_img:getWidth()
+		action_menu_btn[v].h = action_icon_img:getHeight()
+		icon_x = icon_x + action_menu_btn[v].w + icon_distance
+	end
+
+	-- draw action buttons
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.draw(action_icon_img, action_menu_btn["build"].x, action_menu_btn["build"].y)
+	love.graphics.setColor(255, 0, 0)
+	love.graphics.draw(action_icon_img, action_menu_btn["trade"].x, action_menu_btn["trade"].y)
+	love.graphics.setColor(0, 255, 0)
+	love.graphics.draw(action_icon_img, action_menu_btn["develcards"].x, action_menu_btn["develcards"].y)
+	love.graphics.setColor(255, 255, 255)
+
+	-- some distance between action buttons and resource icons
+	icon_x = icon_x + 20
 
 	-- going through resource list
 	for k, v in ipairs(resource_list) do
@@ -43,21 +66,12 @@ function panel.drawBar()
 		love.graphics.print(player[1].resource[v], icon_x, text_y)
 		icon_x = icon_x + love.graphics.newFont():getWidth(player[1].resource[v]) + text_distance
 	end
-
-	x = 110
-	y = love.graphics.getHeight() - 50
-	action_menu_btn["action"] = {}
-	action_menu_btn["action"].x = x
-	action_menu_btn["action"].y = y
-	action_menu_btn["action"].w = 50
-	action_menu_btn["action"].h = 50
-	love.graphics.draw(action_icon_img, x, y)
 end
 
 function panel.mousepressed(x, y, button)
-	if x >= action_menu_btn["action"].x and x <= (action_menu_btn["action"].x + action_menu_btn["action"].w)
-		and y >= action_menu_btn["action"].y and y <= (action_menu_btn["action"].y + action_menu_btn["action"].h) then
-			print("action menu button pressed")
+	if x >= action_menu_btn["build"].x and x <= (action_menu_btn["build"].x + action_menu_btn["build"].w)
+		and y >= action_menu_btn["build"].y and y <= (action_menu_btn["build"].y + action_menu_btn["build"].h) then
+			print("build action menu button pressed")
 			-- treat this as 'build street' button for now
 		    game.action = "street"
 	end
